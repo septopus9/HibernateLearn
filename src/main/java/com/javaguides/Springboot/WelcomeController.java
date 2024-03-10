8d09ecdbe554a4f9552e775ad2a33043c85b01d5
septopus9 <septopus9@gmail.com>
2024-03-10 14:28:09 +0530

    First Commit

---
+++ src/main/java/com/javaguides/Springboot/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.javaguides.Springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+   @GetMapping("/welcome")
+    public String welcome (){
+        return "Spring - booot world !";
+
+
+    }
+}
